475a1d14154970342b2483ef1424d08abc64ef66
Daniel Pihlstrøm Karlsen <danielpk@hiof.no>
2026-04-16 14:54:40 +0200

    Fikset relativ filsti for bilde.

--- frontend/src/components/Daniel.jsx
+++ frontend/src/components/Daniel.jsx
@@ -4,7 +4,7 @@ export default function Daniel()
 {
     return (
         <article>
-        <img src="public/website_images/Daniel_2024.JPG" alt="Daniel" />
+        <img src="/website_images/Daniel_2024.JPG" alt="Daniel" />
         <h2>Daniel Pihlstrom Karlsen</h2>
         <p><a href="mailto:danielpk@hiof.no">danielpk@hiof.no</a></p> 
         <p className="bachelor">Bachelorstudium i informatikk - design og utvikling av IT-systemer</p>
