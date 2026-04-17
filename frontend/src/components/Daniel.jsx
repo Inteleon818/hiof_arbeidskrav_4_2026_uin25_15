e25e81faf8a7080b6a7876beb4b1281a833f4f16
Daniel Pihlstrøm Karlsen <danielpk@hiof.no>
2026-04-17 13:25:50 +0200

    Lagt til litt styling og gjorde noen forandringer på Daniel-komponentet.

--- frontend/src/components/Daniel.jsx
+++ frontend/src/components/Daniel.jsx
@@ -6,8 +6,8 @@ export default function Daniel()
         <article>
         <img src="/website_images/Daniel_2024.JPG" alt="Daniel" />
         <h2>Daniel Pihlstrom Karlsen</h2>
-        <p><a href="mailto:danielpk@hiof.no">danielpk@hiof.no</a></p> 
-        <p className="bachelor">Bachelorstudium i informatikk - design og utvikling av IT-systemer</p>
+        <p>E-post: <a href="mailto:danielpk@hiof.no">danielpk@hiof.no</a></p> 
+        <p className="bachelor">Bachelorstudium: Informatikk - design og utvikling av IT-systemer</p>
         </article>
     )
 }
