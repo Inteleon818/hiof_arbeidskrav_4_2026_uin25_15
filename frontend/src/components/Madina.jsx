cf17c30a2ebf6aeb801e0f945ac3d857459740b1
madinami <hhei544@gmail.com>
2026-04-20 14:50:16 +0200

    css Madina
    ferdig med å fikse css til komponenten på min egne branch

--- frontend/src/components/Madina.jsx
+++ frontend/src/components/Madina.jsx
@@ -6,9 +6,8 @@ export default function Madina (){
     return (<article className="Madina">
         <img src="../website_images/dino.jpg" alt="dinosaur" />
         <h2>Madina Iqbal</h2>
-        <p> <a href="madinami@hiof.no">madinami@hiof.no</a></p>
-        <p>Informasjonssystemer</p>
-
+        <p> <a href="mailto:madinami@hiof.no">madinami@hiof.no</a></p>
+        <p className='madinap'>Informasjonssystemer</p>
     </article>)
 }
 
